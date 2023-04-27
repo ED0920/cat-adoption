7f087f96167084b2a5899417ef54cf6f5b8e5668
Elisa <elisa.do@outlook.com>
2023-04-27 23:48:09 +1000

    create react component for FAQ

---
+++ client/src/pages/Cats.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const body = {
+  minHeight: "85vh",
+  background: "#6461A0",
+  color: "#444c54",
+};
+function Cats() {
+  return (
+    <>
+      <Header />
+      <div style={body}>Cats</div>
+      <Footer />
+    </>
+  );
+}
+
+export default Cats;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const body = {
+  minHeight: "85vh",
+  background: "#6461A0",
+  color: "#444c54",
+};
+
+function Login() {
+  return (
+    <>
+      <Header />
+      <div style={body}>Login</div>
+      <Footer />
+    </>
+  );
+}
+
+export default Login;
