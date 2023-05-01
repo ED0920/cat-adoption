429b137618fb5be09190ca850baf7237bc0b2d67
BhatJ <jisha2209@gmail.com>
2023-05-01 17:33:46 +1000

    Added a query to get all available cats

--- client/src/pages/Cats.jsx
+++ client/src/pages/Cats.jsx
@@ -3,12 +3,21 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
 
+import { useQuery } from '@apollo/client';
+import { QUERY_AVAILABLE_CATS } from '../utils/queries';
+
 const body = {
   minHeight: "65vh",
   background: "#6461A0",
   color: "#444c54",
 };
 function Cats() {
+  const { loading, data } = useQuery(QUERY_AVAILABLE_CATS);
+  const cats = data?.availableCats || [];
+
+  console.log(cats);
+  console.log(loading);
+
   return (
     <>
       <Header />
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,18 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_AVAILABLE_CATS = gql`
+  query getAvailableCats {
+    availableCats {
+      _id
+      name
+      state
+      sex
+      age
+      breed
+      colour
+      personality
+      imgFilename
+      bioText
+    } 
+  }
+`;
