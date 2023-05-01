3757701e2fec355835306e0a7b9ee995bc6f6c9f
BhatJ <jisha2209@gmail.com>
2023-05-01 14:18:19 +1000

    Added missing dependencies for Apollo server, GraphQL, and JWT authorisation
    Added a util folder which has scripts for authorisation, queries, and mutations
    
    Added auth.js script

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import './App.css';
-
 import AboutUs from './pages/AboutUs';
 import Cats from './pages/Cats';
 import ContactUs from './pages/ContactUs';
@@ -9,10 +17,29 @@ import Login from './pages/Login';
 import Rehoming from './pages/Rehoming';
 import Signup from './pages/Signup'
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const router = createBrowserRouter([
   {
@@ -47,11 +74,9 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-
-
-    <RouterProvider router={router} />
-
-
+    <ApolloProvider client={client}>
+      <RouterProvider router={router} />
+    </ApolloProvider>
   );
 }
 
