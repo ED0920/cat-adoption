97b71b92f6da3669a1bbae56e1d7ee461fee49a3
BhatJ <jisha2209@gmail.com>
2023-05-01 15:29:15 +1000

    Added mutation to log a user into the app
    Updated the login script to use the mutation
    
    Updated header component to use login information to render login/logout/sign-up elements

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,7 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Auth from '../utils/auth';
 
 const NavBar = {
     display: 'flex',
@@ -30,20 +33,30 @@ const navStyle = {
 
 const Header = () => {
 
+    const logout = (event) => {
+      event.preventDefault();
+      Auth.logout();
+    };
 
     return (
         <div className="Nav" style={NavBar}>
 
             <img style={icon} src={require('../assets/another.png')} alt="logo" />
 
-
-
             <div className="Nav" style={Nav}>
                 <Link to={"/"} style={navStyle}> About Us </Link>
                 <Link to={"/cats"} style={navStyle}>Cats Available for Adoption</Link>
                 <Link to={"/rehome"} style={navStyle}>Rehoming Services</Link>
-                <Link to={"/login"} style={navStyle}>Login</Link>
-                <Link to={"/signup"} style={navStyle}>Sign Up</Link>
+                {Auth.loggedIn() ? (
+                    <>
+                        <a style={navStyle} onClick={logout} href="#">Logout</a>
+                    </>
+                ) : (
+                    <>
+                        <Link to={"/login"} style={navStyle}>Login</Link>
+                        <Link to={"/signup"} style={navStyle}>Sign Up</Link>
+                    </>
+                )}
             </div>
         </div >
     )
